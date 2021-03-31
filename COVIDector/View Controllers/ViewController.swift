d6b74c9b001d5753359410cd539ffbf3c9e18c28
Kapil Balagopal <kapiloneness@gmail.com>
2021-03-31 21:50:21 +0530

    - Changed Home page Input data button shapes to circle from default iOS style shape.

--- COVIDector/View Controllers/ViewController.swift
+++ COVIDector/View Controllers/ViewController.swift
@@ -27,21 +27,26 @@ class ViewController: UIViewController {
         let buttonFontSize = 26 * (DeviceSize.deviceWidth / DeviceSize.baseWidth)
         
         enterClinicalData.setTitle("Enter Clinical Vitals", for: .normal)
+        enterClinicalData.titleLabel?.numberOfLines = 0
+        enterClinicalData.titleLabel?.textAlignment = .center
         enterClinicalData.titleLabel?.font = .systemFont(ofSize: buttonFontSize)
         enterClinicalData.setTitleColor(.white, for: .normal)
         enterClinicalData.backgroundColor = .systemBlue
-        enterClinicalData.frame = CGRect(x: DeviceSize.deviceWidth * 0.05, y: self.view.frame.height / 2 - DeviceSize.deviceHeight * 0.1, width: DeviceSize.deviceWidth * 0.9 , height: enterClinicalData.intrinsicContentSize.height)
-        enterClinicalData.layer.cornerRadius = enterClinicalData.frame.height * 0.2
+        enterClinicalData.frame = CGRect(x: DeviceSize.deviceWidth / 2 - DeviceSize.deviceWidth * 0.2, y: self.view.frame.height / 2 - DeviceSize.deviceHeight * 0.2, width: DeviceSize.deviceWidth * 0.4, height: DeviceSize.deviceWidth * 0.4)
+        enterClinicalData.layer.cornerRadius = enterClinicalData.frame.width * 0.5
         enterClinicalData.layer.masksToBounds = false
         enterClinicalData.addTarget(self, action: #selector(goToEnterClinicalData), for: .touchUpInside)
         self.view.addSubview(enterClinicalData)
         
         uploadChestImage.setTitle("Upload X-ray Image", for: .normal)
+        uploadChestImage.titleLabel?.numberOfLines = 0
+        uploadChestImage.titleLabel?.textAlignment = .center
         uploadChestImage.titleLabel?.font = .systemFont(ofSize: buttonFontSize)
         uploadChestImage.setTitleColor(.white, for: .normal)
         uploadChestImage.backgroundColor = .systemBlue
-        uploadChestImage.frame = CGRect(x: DeviceSize.deviceWidth * 0.05, y: self.view.frame.height / 2 + DeviceSize.deviceHeight * 0.1, width: DeviceSize.deviceWidth * 0.9 , height: enterClinicalData.intrinsicContentSize.height)
-        uploadChestImage.layer.cornerRadius = uploadChestImage.frame.height * 0.2
+        uploadChestImage.frame = CGRect(x: DeviceSize.deviceWidth / 2 - DeviceSize.deviceWidth * 0.2, y: self.view.frame.height / 2 + DeviceSize.deviceHeight * 0.1, width: DeviceSize.deviceWidth * 0.4, height: DeviceSize.deviceWidth * 0.4)
+        //  uploadChestImage.frame = CGRect(x: DeviceSize.deviceWidth * 0.05, y: self.view.frame.height / 2 + DeviceSize.deviceHeight * 0.1, width: DeviceSize.deviceWidth * 0.9, height: enterClinicalData.intrinsicContentSize.height)
+        uploadChestImage.layer.cornerRadius = uploadChestImage.frame.width * 0.5// uploadChestImage.frame.height * 0.2
         uploadChestImage.layer.masksToBounds = false
         uploadChestImage.addTarget(self, action: #selector(goToUploadXrayImage), for: .touchUpInside)
         self.view.addSubview(uploadChestImage)
