6842ac68891fbb82dc5129e73504835f84612e1c
Kapil Balagopal <kapiloneness@gmail.com>
2021-04-01 17:15:31 +0530

    - Added info page content

--- COVIDector/View Controllers/InfoViewController.swift
+++ COVIDector/View Controllers/InfoViewController.swift
@@ -33,7 +33,7 @@ class InfoViewController: UIViewController {
         infoTextView.textAlignment = .left
         infoTextView.isEditable = false
         infoTextView.font = .systemFont(ofSize: inforTextFontSize)
-        infoTextView.text = "Swipe up to change the shape, swipe down to change the color. \n \nYour main aim is to avoid obstacles. The Bar obstacle (Orange colored) can be avoided in any form, color or shape. In the Classic Mode, you must either match the shape or color to overcome the Target obstacle (Purple colored, along with a shape). \n \nBONUS: You will earn Bonus points when you match the shape and color when encoutering a Shape obstacle in this mode!"
+        infoTextView.text = "COVIDector is an app which can predict whether a patient is Negative/Positive for COVID-19, based on AI. \nIt can take two kinds of data, patient's vitals (Haemoglobin, Oxygen Percent, Platelets Count, etc.) or the X-ray image of the patient's lungs. The app after running some Machine/Deep Learning calculations on these data, retuns a Positive/Negative label along with the confidence or probability of the respective prediction. \n \nNOTE: We are not trying to replace any existing gold standard methods of COVID-19 detection, rather we just want to add a complementary option of detection in conjunction with the existing solutions."
         infoTextView.frame = CGRect(x: DeviceSize.deviceWidth * 0.05, y: titleLabel.frame.maxY + DeviceSize.deviceHeight * 0.05, width: DeviceSize.deviceWidth * 0.9, height: 0)
         infoTextView.sizeToFit()
         self.view.addSubview(infoTextView)
